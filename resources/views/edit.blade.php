c216fa85ee2fcd178111bb14e64529d2b2f044d9
maya-nakamura <maya2021se@gmail.com>
2021-07-23 22:19:07 +0900

    210723 レイアウト

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -2,14 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Todo</title>
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        <title style="text-align:center">Todo</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
         <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
         <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/ja.js"></script>
     </head>
     
     <body>
-        <h1 class="title">Edit Paged</h1>
+        <h1 class="title" style="text-align:center">Edit Paged</h1>
         
         <div class="content">
             
@@ -18,38 +19,38 @@
                 @method('PUT')
                 
                 <div class="content_title">
-                    <h2>Title</h2>
+                    <h2 style="text-align:center">Title</h2>
                     <input type="text" name="todo[title]" placeholder="task" value="{{ $todo->title }}"/>
-                    <p class="title__error" style="color:red">{{ $errors->first('todo.title') }}</p>
+                    <p style="text-align:center" class="title__error" style="color:red">{{ $errors->first('todo.title') }}</p>
                 </div>
                 
                 <div class="content_tag">
-                    <h2>Tag</h2>
+                    <h2 style="text-align:center">Tag</h2>
                     @foreach($todo->tags as $tag)
                         <input type="text" name="tag[tagname]" placeholder="tag" value="{{ $tag->tagname }}"/>
-                        <p class="tag__error" style="color:red">{{ $errors->first('tag.tagname') }}</p>
+                        <p style="text-align:center" class="tag__error" style="color:red">{{ $errors->first('tag.tagname') }}</p>
                     @endforeach
                 </div>
                 
                 <div class="content_body">
-                    <h2>Details</h2>
+                    <h2 style="text-align:center">Details</h2>
                     <input type="text" name="todo[body]" placeholder="memo" value="{{ $todo->body }}"/>
-                    <p class="body__error" style="color:red">{{ $errors->first('todo.body') }}</p>
+                    <p  style="text-align:center"class="body__error" style="color:red">{{ $errors->first('todo.body') }}</p>
                 </div>
                 
                 <div class="content_deadline">
-                    <h2>Deadline</h2>
+                    <h2 style="text-align:center">Deadline</h2>
                     <input class="flatpickr" type="text" name="todo[deadline]" placeholder="Select Date.." value="{{ $todo->deadline }}"/>
-                    <p class="deadline__error" style="color:red">{{ $errors->first('todo.deadline') }}</p>
+                    <p style="text-align:center" class="deadline__error" style="color:red">{{ $errors->first('todo.deadline') }}</p>
                     <script>
                         flatpickr('.flatpickr');
                     </script>
                 </div>
                 
-                <input type="submit" value="serve"/>
+                <input style="text-align:center" type="submit" value="serve"/>
             </form>
         </div>
-        <div class="back">[<a href="/">back</a>]</div>
+        <div style="text-align:center" class="back">[<a href="/">back</a>]</div>
     </body>
     
 </html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,34 +2,35 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>TodoList</title>
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        <title style="text-align:center">TodoList</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     
     <body>
-        <h1>Todo</h1>
-        <p class='edit'>[<a href="/todos/{{ $todo->id }}/edit">edit</a>]</p>
+        <h1 style="text-align:center">Todo</h1>
+        <p style="text-align:center" class='edit'>[<a href="/todos/{{ $todo->id }}/edit">edit</a>]</p>
         
         <form action="/todos/{{ $todo->id }}" id="from_delete" method="POST">
             @csrf
             @method('DELETE')
             <input type="submit" style="display:none"/>
-            <p class='delete'>[<span onclick="deleteTodo(this)">delete</span>]</p> 
+            <p style="text-align:center" class='delete'>[<span onclick="deleteTodo(this)">delete</span>]</p> 
         </form>
         
         <div class='contents'>
-            <h2>{{ $todo->title }}</h2>
-            <p>
+            <h2 style="text-align:center">{{ $todo->title }}</h2>
+            <p style="text-align:center">
                 @foreach($todo->tags as $tag)
                     {{$tag->tagname}}
                 @endforeach
             </p>
-            <p>{{ $todo->body }}</p>
-            <p>{{ $todo->deadline }}</p>
+            <p style="text-align:center">{{ $todo->body }}</p>
+            <p style="text-align:center">{{ $todo->deadline }}</p>
         </div>
         
-        <div class='back'>[<a href='/'>back</a>]</div>
+        <div style="text-align:center" class='back' style="text-align:center">[<a href='/'>back</a>]</div>
         
         <script>
         function deleteTodo(e){
